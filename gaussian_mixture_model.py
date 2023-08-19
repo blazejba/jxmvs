3949ef1930fe841e1e34522c33912270ff037ae0
Blazej Banaszewski <blazejb@unclassified.ai>
2023-08-19 22:10:53 +0100

    benchmark jax on cpu/gpu backend and with/without jit

--- gaussian_mixture_model.py
+++ gaussian_mixture_model.py
@@ -1,4 +1,5 @@
 import jax
+import time
 import jax.numpy as jnp
 from jax.scipy.stats import multivariate_normal
 from jax import random
@@ -49,6 +50,23 @@ def e_step(data, pi, mu, sigma):
     return responsibilities
 
 
+@jax.jit
+def update_covariance(data, mu_k, responsibilities_k):
+    """
+    Helper function to compute the updated covariance matrix for a single cluster.
+    data: Input data, shape (N, D)
+    mu_k: Mean of the Gaussian for cluster k, shape (D,)
+    responsibilities_k: Posterior probabilities for cluster k, shape (N,)
+    
+    Returns:
+    sigma_k: Updated covariance matrix for cluster k, shape (D, D)
+    """
+    diff = data - mu_k
+    sigma_k = jnp.dot(responsibilities_k * diff.T, diff) / jnp.sum(responsibilities_k)
+    return sigma_k
+
+
+@jax.jit
 def m_step(data, responsibilities):
     """
     M-step: Update the parameters.
@@ -70,12 +88,8 @@ def m_step(data, responsibilities):
     mu = jnp.dot(responsibilities.T, data) / jnp.sum(responsibilities, axis=0)[:, jnp.newaxis]
     
     # Update covariance matrices
-    sigma = jnp.zeros((K, D, D))
-    for k in range(K):
-        diff = data - mu[k]
-        sigma_k = jnp.dot(responsibilities[:, k] * diff.T, diff) / jnp.sum(responsibilities[:, k])
-        sigma = sigma.at[k].add(sigma_k)
-        
+    sigma = jnp.stack([update_covariance(data, mu[k], responsibilities[:, k]) for k in range(K)])
+    
     return pi, mu, sigma
 
 
@@ -92,10 +106,10 @@ def gmm_em(data, K, max_iters=100, tol=1e-4):
     mu: Final means of the Gaussians, shape (K, D)
     sigma: Final covariance matrices of the Gaussians, shape (K, D, D)
     """
-    N, D = data.shape
+    _, D = data.shape
     pi, mu, sigma = initialize_gmm_params(K, D)
     
-    for iter in range(max_iters):
+    for _ in range(max_iters):
         # E-step
         responsibilities = e_step(data, pi, mu, sigma)
         
@@ -110,6 +124,7 @@ def gmm_em(data, K, max_iters=100, tol=1e-4):
         
     return pi, mu, sigma
 
+
 def plot_gmm(data, pi, mu, sigma, filename='gmm_clustering.png'):
     """
     Plot the GMM model along with the data and save the figure to a file.
@@ -117,7 +132,7 @@ def plot_gmm(data, pi, mu, sigma, filename='gmm_clustering.png'):
     pi: Mixing proportions, shape (K,)
     mu: Means of the Gaussians, shape (K, D)
     sigma: Covariance matrices of the Gaussians, shape (K, D, D)
-    filename: Name of the file to save the plot, with file extension (e.g., 'gmm_clustering.png')
+    filename: Name of the file to save the plot, with file extension
     """
     plt.scatter(data[:, 0], data[:, 1], c='blue', s=5, label='Data Points')
     
@@ -141,18 +156,62 @@ def plot_gmm(data, pi, mu, sigma, filename='gmm_clustering.png'):
     
     print(f"Figure saved to {filename}")
 
-# Test data generation (2D points)
-N = 300  # Number of data points
-D = 2  # Dimensionality of the data
-K = 3  # Number of clusters
 
-# Generate synthetic data for testing
-true_mu = jnp.array([[2.0, 3.0], [7.0, 5.0], [6.0, 10.0]])
-true_sigma = jnp.array([[[1.0, 0.5], [0.5, 1.0]], [[1.5, 0.2], [0.2, 0.8]], [[1.0, -0.6], [-0.6, 1.0]]])
-data = jnp.vstack([random.multivariate_normal(random.PRNGKey(k), true_mu[k], true_sigma[k], (N // K,)) for k in range(K)])
+def main():
+    # Test data generation (2D points)
+    N = 300  # Number of data points
+    D = 2  # Dimensionality of the data
+    K = 3  # Number of clusters
 
-# Fit the GMM model
-pi, mu, sigma = gmm_em(data, K)
+    # Generate synthetic data for testing
+    true_mu = jnp.array([
+        [2.0, 3.0],
+        [7.0, 5.0],
+        [6.0, 10.0]
+    ])
+    true_sigma = jnp.array([
+        [[1.0,  0.5], [ 0.5, 1.0]],
+        [[1.5,  0.2], [ 0.2, 0.8]],
+        [[1.0, -0.6], [-0.6, 1.0]]
+    ])
+    data = jnp.vstack(
+        [random.multivariate_normal(random.PRNGKey(k), true_mu[k], true_sigma[k],(N // K,))
+         for k in range(K)])
 
-# Plot the GMM along with the data
-plot_gmm(data, pi, mu, sigma)
+    def time_gmm(backend, jit_compile):
+        jax.config.update("jax_platform_name", backend)
+        
+        start_time = time.time()
+        
+        if jit_compile:
+            gmm_em(data, K)
+        else:
+            with jax.disable_jit():
+                gmm_em(data, K)
+        
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        
+        print(
+            f"Backend: {backend}, "
+            f"JIT: {'Yes' if jit_compile else 'No'}, "
+            f"Time: {elapsed_time:.5f} seconds"
+        )
+
+
+    # Timing the code for different combinations
+    for backend in ["cpu", "gpu"]:
+        for jit_compile in [False, True]:
+            time_gmm(backend, jit_compile)
+            
+    # Fit the GMM model
+    # pi, mu, sigma = gmm_em(data, K)
+
+    # Plot the GMM along with the data
+    # plot_gmm(data, pi, mu, sigma)
+
+
+
+
+if __name__ == '__main__':
+    main()
